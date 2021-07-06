a9f9201d34d88396b2140cce237b93eb6f93fd93
Sachan75 <merry.bell0705@gmail.com>
2021-07-06 20:08:40 +0900

    🥕とりあえず積みあがるようにはなりました

--- Assets/Scripts/Puyo.cs
+++ Assets/Scripts/Puyo.cs
@@ -6,7 +6,7 @@ using manpuku_kitchen.Utils;
 public class Puyo : MonoBehaviour
 {
     GameObject[] puyos;
-    GameObject Director;
+    //GameObject Director;
     public int num = 0;    //num=0で落下し、終了時にfallcheckへ、num=1なら休止
     float[] puyox = new float[100];
     float[] puyoy = new float[100];
@@ -20,7 +20,7 @@ public class Puyo : MonoBehaviour
     public Ingredients ingredient;
 
     // minoの落ちる時間
-    public float fallTime = 1f;
+    public float fallTime = 5.0f;
     //落下完了フラグ
     public int fallCompFlg = 0;
 
@@ -33,55 +33,47 @@ public class Puyo : MonoBehaviour
 
     void Update()
     {
-        int i = 0;
-        this.Director = GameObject.Find("Director");
-        this.puyos = GameObject.FindGameObjectsWithTag("puyo");
-        foreach (GameObject puyo in this.puyos)
-        {
-            //丸め誤差解消（フィールト中の全ぷよの位置）
-            this.puyox[i] = Mathf.RoundToInt(puyo.transform.position.x * 10.0f) / 10.0f;
-            this.puyoy[i] = Mathf.RoundToInt(puyo.transform.position.y * 10.0f) / 10.0f;
-            i++;
-        }
+        
 
         MinoMovememt();
 
-        i = 0;
-        //丸め誤差解消（自分の今の位置）
-        double nowx = Mathf.RoundToInt(transform.position.x * 10.0f) / 10.0f;
-        double nowy = Mathf.RoundToInt(transform.position.y * 10.0f) / 10.0f;
+        //i = 0;
+        ////丸め誤差解消（自分の今の位置）
+        //double nowx = Mathf.RoundToInt(transform.position.x * 10.0f) / 10.0f;
+        //double nowy = Mathf.RoundToInt(transform.position.y * 10.0f) / 10.0f;
 
-        if (this.num == 1) return;  //落下完了済なので以下の処理不要
-        if (transform.root.gameObject == gameObject)
-        {
-            //コンビ解散後の挙動を記述
-            if (nowy == 1.5f)
-            {
-                this.num = 1;   //落下完了をお知らせ
-                FindObjectOfType<Delete>().init();
-                FindObjectOfType<Delete>().puyoDestroy();
-                return;
-            }
-            i = 0;
-            foreach (GameObject puyo in this.puyos)
-            {
-                if (nowx == this.puyox[i] && nowy == this.puyoy[i] + 1.0f)
-                {
-                    this.num = 1;   //落下完了をお知らせ
-                    FindObjectOfType<Delete>().init();
-                    FindObjectOfType<Delete>().puyoDestroy();
-                    return;
-                }
-                i++;
-            }
-            //落下完了していないので引き続き落下
-            transform.Translate(0, -1.0f, 0, Space.World);
-        }
+        //if (this.num == 1) return;  //落下完了済なので以下の処理不要
+        //if (transform.root.gameObject == gameObject)
+        //{
+        //    //コンビ解散後の挙動を記述
+        //    if (nowy == 1.5f)
+        //    {
+        //        this.num = 1;   //落下完了をお知らせ
+        //        FindObjectOfType<Delete>().init();
+        //        FindObjectOfType<Delete>().puyoDestroy();
+        //        return;
+        //    }
+        //    i = 0;
+        //    foreach (GameObject puyo in this.puyos)
+        //    {
+        //        if (nowx == this.puyox[i] && nowy == this.puyoy[i] + 1.0f)
+        //        {
+        //            this.num = 1;   //落下完了をお知らせ
+        //            FindObjectOfType<Delete>().init();
+        //            FindObjectOfType<Delete>().puyoDestroy();
+        //            return;
+        //        }
+        //        i++;
+        //    }
+        //    //落下完了していないので引き続き落下
+        //    transform.Translate(0, -1.0f, 0, Space.World);
+        //}
 
     }
 
     private void MinoMovememt()
     {
+        //着地前の単体食材の動き
         if (transform.root.gameObject != gameObject)
         {
             if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -98,18 +90,25 @@ public class Puyo : MonoBehaviour
 
             }
         }
+        //着地後の動き
         else if (transform.root.gameObject == gameObject)
         {
+           
             if (Time.time - previousTime >= fallTime)
             {
-                transform.position += new Vector3(0, -1, 0);
+                Observer();
 
+                //下に食材があるorエリアの下端まで来た時
                 if (!ValidMovement())
                 {
-                    transform.position -= new Vector3(0, -1, 0);
-                    //fallCompFlg = 1;
-                    this.enabled = false;
-
+                    FindObjectOfType<Delete>().init();
+                    FindObjectOfType<Delete>().puyoDestroy();
+                    //this.enabled = false;
+                }
+                //まだ下に移動できるとき
+                else if(ValidMovement())
+                {
+                    transform.position += new Vector3(0, -1, 0);
                 }
 
                 previousTime = Time.time;
@@ -122,16 +121,16 @@ public class Puyo : MonoBehaviour
     bool ValidMovement()
     {
 
-        double roundX = Mathf.RoundToInt(transform.position.x);
-        double roundY = Mathf.RoundToInt(transform.position.y);
+        double roundX = Mathf.RoundToInt(transform.position.x * 10.0f) / 10.0f;
+        double roundY = Mathf.RoundToInt(transform.position.y * 10.0f) / 10.0f;
 
         // minoがステージよりはみ出さないように制御
-        if (roundX <= 4.0 || roundX >= 11.0 || roundY <= 1.0)
+        if (roundX <= 4.0 || roundX >= 11.0 || roundY <= 2.0)
         {
             return false;
         }
 
-        this.puyos = GameObject.FindGameObjectsWithTag("puyo");
+        //this.puyos = GameObject.FindGameObjectsWithTag("puyo");
 
         int i = 0;
         foreach (GameObject puyo in this.puyos)
@@ -143,7 +142,8 @@ public class Puyo : MonoBehaviour
 
         }
         i = 0;
-        foreach (float x in this.puyox) {
+        foreach (float x in this.puyox)
+        {
 
             //落下終了条件
             if (roundX == this.puyox[i] && roundY == this.puyoy[i] + 1.0f)
@@ -154,6 +154,20 @@ public class Puyo : MonoBehaviour
         }
 
         return true;
+    }
+
+    private void Observer() {
+
+        int i = 0;
+        this.puyos = GameObject.FindGameObjectsWithTag("puyo");
+        foreach (GameObject puyo in this.puyos)
+        {
+            //丸め誤差解消（フィールト中の全ぷよの位置）
+            this.puyox[i] = Mathf.RoundToInt(puyo.transform.position.x * 10.0f) / 10.0f;
+            this.puyoy[i] = Mathf.RoundToInt(puyo.transform.position.y * 10.0f) / 10.0f;
+            i++;
+        }
+
     }
 
 
